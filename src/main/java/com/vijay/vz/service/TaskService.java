2f8904a44a0323592ce4b640944b9c38a97ea2d7
Kumar Vijay <vijay81238@gmail.com>
2024-11-20 19:22:24 +0530

    Added logger implementation

--- src/main/java/com/vijay/vz/service/TaskService.java
+++ src/main/java/com/vijay/vz/service/TaskService.java
@@ -4,6 +4,8 @@ import com.vijay.vz.entity.Task;
 import com.vijay.vz.exception.BadRequestException;
 import com.vijay.vz.exception.ResourceNotFoundException;
 import com.vijay.vz.repository.TaskRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,11 +16,14 @@ public class TaskService {
 
     TaskRepository taskRepository;
 
+    private static  final Logger logger = LoggerFactory.getLogger(TaskService.class);
+
     public TaskService(TaskRepository taskRepository){
         this.taskRepository=taskRepository;
     }
 
     public Task createTask(Task task){
+        logger.info("Creating new task:{} ",task.getTitle());
         if(task.getTitle()==null || task.getTitle().isBlank()){
             throw new BadRequestException("Task title can't be empty");
         }
@@ -26,10 +31,12 @@ public class TaskService {
     }
 
     public List<Task> getAllTasks(){
+        logger.info("Fetching all tasks");
         return taskRepository.findAll();
     }
 
     public Task updateTask(String id,Task updatedTask){
+        logger.info("Update task : {} ",updatedTask.getTitle());
         Optional<Task> optionalTask= taskRepository.findById(id);
 
         if(optionalTask.isPresent()){
@@ -41,15 +48,17 @@ public class TaskService {
             return taskRepository.save(task);
 
         }
+        logger.error("Task with ID: {} not found to update",id );
         throw new RuntimeException("Task not found");
     }
 
     public void deleteTask(String id){
+        logger.warn("Deleting task with ID: {}", id);
         taskRepository.deleteById(id);
-        System.out.println("I am after deleteing....");
     }
 
     public List<Task> getPendingTasks(){
+        logger.info("Fetching pending tasks");
         Optional<List<Task>> allTask= Optional.of(taskRepository.findAll());
         List<Task> pendingTasks = null;
         if(allTask.isPresent()){
@@ -59,9 +68,13 @@ public class TaskService {
     }
 
     public Task getTaskById(String id) {
-
-        return taskRepository.findById(id).orElseThrow( () -> new ResourceNotFoundException("Task with id - "+id+" not found"));
+        logger.debug("Fetching task with ID:{},", id);
+        return taskRepository.findById(id).orElseThrow(() -> {
+            logger.error("Task with ID {} not found", id);
+            return new ResourceNotFoundException("Task with id - " + id + " not found");
+        });
     }
+
 
     /*public Task getTaskById(String id) {
         Optional<Task> optionalTask= taskRepository.findById(id);
